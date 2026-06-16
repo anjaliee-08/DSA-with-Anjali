a0067b842dcc2c012e40a06e8ae4fd22203954e9
Anjali Gupta <193569297+anjaliee-08@users.noreply.github.com>
2026-06-16 15:36:04 +0530

    cousininabinarytree

---
+++ Trees/Binary tree/cousininabinarytree.java
@@ -0,0 +1,42 @@
+class Solution {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        if(root==null) return root;
+        Queue<TreeNode> q=new LinkedList<>();
+        ArrayList<Integer> levelsum=new ArrayList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int n=q.size();
+            int currsum=0;
+            while(n>0){
+                TreeNode node=q.poll();
+                currsum+=node.val;
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null)q.offer(node.right);
+                n--;
+            }
+            levelsum.add(currsum);
+        }
+        int i=1;
+        q.offer(root);
+        root.val=0;
+        while(!q.isEmpty()){
+            int n=q.size();
+            while(n>0){
+                TreeNode node=q.poll();
+                int cousinsum=node.left!=null?node.left.val:0;
+                cousinsum+=node.right!=null?node.right.val:0;
+                if(node.left!=null){
+                    node.left.val=levelsum.get(i)-cousinsum;
+                    q.offer(node.left);
+                }
+                if(node.right!=null){
+                    node.right.val=levelsum.get(i)-cousinsum;
+                    q.offer(node.right);
+                }
+                n--;
+            }
+            i++;
+        }
+        return root;
+    }
+}
